8e2d4ec7555b0cf6beacb35e680da1b6f5f94f01
wingled22 <pelayowindel@gmail.com>
2023-03-11 10:04:44 +0800

    kanang daghan nag edit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,38 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+
+        Person p = new Person();
+        p.ID= 1;
+        p.Fullname = "windel";
+
+        return View(p);
     }
 
     public IActionResult Privacy()
     {
-        return View();
+
+        
+        Person p = new Person();
+        p.ID= 1;
+        p.Fullname = "windel";
+
+        Person p2 = new Person();
+        p2.ID= 2;
+        p2.Fullname = "JEng";
+
+        Person p3 = new Person();
+        p3.ID= 2;
+        p3.Fullname = "teletubies";
+
+        List<Person> list = new List<Person>();
+
+        list.Add(p);
+        list.Add(p2);
+        list.Add(p3);
+
+
+        return View(list);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Person.cs
@@ -0,0 +1,8 @@
+namespace pelayoApp.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Fullname {get; set;}
+    }
+}
